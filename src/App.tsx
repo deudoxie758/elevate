530acfeb732c6966428661b1db8ee4aa3e6ec78d
Darnell Eudoxie <darnelleudoxie@gmail.com>
2023-04-13 12:36:13 -0400

    initial skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: React.FC = () => {
+
+  
+
+
+return (
+  <div className="App">
+    <h1>Giphy Search</h1>
+    <form>
+      <input type="text" name="search" placeholder="Search GIF's"/>
+      <button type="submit">Search</button>
+    </form>
+    <div>
+      {/* Gifs go here */}
     </div>
-  );
+  </div>
+)
 }
 
-export default App;
+export default App
